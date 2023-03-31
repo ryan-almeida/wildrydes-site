87ced1b85cf6bb1a9a1917e13de5ce752ae846a3
Ryan Almeida <ryanalmeida@hotmail.com>
2023-03-31 12:14:21 -0400

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_8WJYNpCP3', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '60dpb0bvenjtne7sc3itfg6inh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
